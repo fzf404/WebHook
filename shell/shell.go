6a569bdea925856e0c4f0624c01482cd9cb2fc1d
fzf404 <nmdfzf404@163.com>
2021-08-11 17:43:41 +0800

    🎨 shell文件报错日志

--- shell/shell.go
+++ shell/shell.go
@@ -17,15 +17,24 @@ func ShellRunner(shellPath string, succLoger *log.Logger, errLoger *log.Logger) 
 	// 执行
 	cmd := exec.Command("/bin/bash", shellPath)
 	stdout, _ := cmd.StdoutPipe()
+	stderr, _ := cmd.StderrPipe()
+
 	err := cmd.Start()
 	if err != nil {
-		errLoger.Print("🚨 Shell Run Error: ", err.Error())
-		log.Fatal("🚨 Shell Run Error.")
+		errLoger.Print("🚨 Shell Start Error: ", err.Error())
+		log.Fatal("🚨 Shell Start Error.")
 	}
+
 	// 读输出
 	bytes, _ := ioutil.ReadAll(stdout)
+	errbytes, _ := ioutil.ReadAll(stderr)
+
+	if len(errbytes) != 0 {
+		log.Print("🚨 Shell Run Error.")
+		errLoger.Print("🚨 Shell Run Error: ", string(errbytes))
+		return
+	}
 
 	log.Print("👍 Shell Run Success.")
-
 	succLoger.Print("👍 Shell Run Success: ", string(bytes))
 }
